8ce19f2296210df6d66d7642fe7d64577760f206
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 19:50:19 -0300

    EndPoint de cadastro de investimento criado.

--- src/main/java/br/com/zup/InvestimentoZup/Investimento/Controller.java
+++ src/main/java/br/com/zup/InvestimentoZup/Investimento/Controller.java
@@ -1,6 +1,8 @@
 package br.com.zup.InvestimentoZup.Investimento;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,11 @@ public class Controller {
 
     @Autowired
     private Service service;
+
+    @PostMapping
+    public Investimento saveInvestimento(@RequestBody Investimento investimento){
+        return service.saveInvestimento(investimento);
+    }
+
 
 }
