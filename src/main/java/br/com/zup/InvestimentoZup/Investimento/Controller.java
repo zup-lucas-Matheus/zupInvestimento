534ee5efe8d1cd4db7754cfe3fa62a7eb3e8ed83
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 19:31:53 -0300

    Classe Controller criada

---
+++ src/main/java/br/com/zup/InvestimentoZup/Investimento/Controller.java
@@ -0,0 +1,11 @@
+package br.com.zup.InvestimentoZup.Investimento;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/invest")
+public class Controller {
+
+
+}
