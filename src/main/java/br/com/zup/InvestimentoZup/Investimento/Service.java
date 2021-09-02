a945686139a4e9914a3c4247bb1967b8173e16fc
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 20:14:25 -0300

    Metódo para cadastrar investimento atualizado onde ele retorna o rendimento do valor aplicado já com o calculo.

--- src/main/java/br/com/zup/InvestimentoZup/Investimento/Service.java
+++ src/main/java/br/com/zup/InvestimentoZup/Investimento/Service.java
@@ -8,14 +8,15 @@ public class Service {
     private Repository repository;
 
     //Metódo cadastrar investimento.
-    public Investimento saveInvestimento(Investimento investimento){
-        return repository.save(investimento);
+    public double saveInvestimento(Investimento investimento){
+         repository.save(investimento);
+         return simularInvestimento(investimento);
     }
 
-
+    //Metódo para simular o investimento do aplicado.
     public double simularInvestimento(Investimento investimento){
         double porcetagem = 0.5;
-        return investimento.getMoneyAplicado() * ((porcetagem / 100)) * investimento.getMeses();
+        return ((investimento.getMoneyAplicado() * porcetagem / 100)  * investimento.getMeses());
 
     }
 
