f5feb87f95e76ef51883595c6175219316d0bb00
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 20:10:53 -0300

    Metódo para converter dto's modelMapper com o @Bean.

--- src/main/java/br/com/zup/InvestimentoZup/componente/Conversor.java
+++ src/main/java/br/com/zup/InvestimentoZup/componente/Conversor.java
@@ -1,7 +1,15 @@
 package br.com.zup.InvestimentoZup.componente;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
 
 @Component
 public class Conversor {
+
+    @Bean
+    public ModelMap modelMap(){
+        return new ModelMap();
+    }
+
 }
