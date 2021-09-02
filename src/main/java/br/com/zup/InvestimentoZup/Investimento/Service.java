e1b258321190f6af9ea0733dcc59b2565c0fc402
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 19:02:35 -0300

    Adicionando a injeção da minha repository na minha classe service

--- src/main/java/br/com/zup/InvestimentoZup/Investimento/Service.java
+++ src/main/java/br/com/zup/InvestimentoZup/Investimento/Service.java
@@ -1,9 +1,11 @@
 package br.com.zup.InvestimentoZup.Investimento;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @org.springframework.stereotype.Service
 public class Service {
-
-
+    @Autowired
+    private Repository repository;
 
 
 }
