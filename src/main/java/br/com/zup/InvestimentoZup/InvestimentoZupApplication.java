d63896ee40242841fba82ffd704aa29d5f84ef89
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 18:47:11 -0300

    Classe investimento criada com todos os atributos.

---
+++ src/main/java/br/com/zup/InvestimentoZup/InvestimentoZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.InvestimentoZup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentoZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentoZupApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/zup/InvestimentoZup/domain/Investimento.java
@@ -0,0 +1,27 @@
+package br.com.zup.InvestimentoZup.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "investimentos")
+public class Investimento {
+
+    @Id
+    private String email;
+    private String nome;
+    private String telefone;
+    private Long moneyAplicado;
+    private int meses;
+
+
+}
