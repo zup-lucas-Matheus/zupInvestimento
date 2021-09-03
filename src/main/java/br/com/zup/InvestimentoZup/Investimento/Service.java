7878e3ec47018564a98dcfe281fdceb7ef89ffcb
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 21:05:07 -0300

    atualização de metódo, com a condição de você só pode aplicar um investimento se o valor for acima de mil reais.

--- src/main/java/br/com/zup/InvestimentoZup/Investimento/Service.java
+++ src/main/java/br/com/zup/InvestimentoZup/Investimento/Service.java
@@ -2,21 +2,31 @@ package br.com.zup.InvestimentoZup.Investimento;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
+
 @org.springframework.stereotype.Service
 public class Service {
     @Autowired
     private Repository repository;
 
     //Metódo cadastrar investimento.
-    public double saveInvestimento(Investimento investimento){
+    public double saveInvestimento(Investimento investimento) throws Exception {
          repository.save(investimento);
+         investimento.getDataDeEntrada();
          return simularInvestimento(investimento);
     }
 
     //Metódo para simular o investimento do aplicado.
-    public double simularInvestimento(Investimento investimento){
-        double porcetagem = 0.5;
-        return ((investimento.getMoneyAplicado() * porcetagem / 100)  * investimento.getMeses());
+    public double simularInvestimento(Investimento investimento) throws Exception {
+        if (investimento.getMoneyAplicado() < 1000) {
+            throw new Exception("Valor de investimento não pode ser menor que R$ 1.000");
+        }
+        else {
+            double porcetagem = 0.5;
+            return ((investimento.getMoneyAplicado() * porcetagem / 100)  * investimento.getMeses());
+        }
+
+
 
     }
 
