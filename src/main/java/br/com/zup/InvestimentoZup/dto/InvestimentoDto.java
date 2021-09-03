1ce64a9480a40fb3680104ff351f2923af914105
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-02 21:58:24 -0300

    Implementando as validações.

--- src/main/java/br/com/zup/InvestimentoZup/dto/InvestimentoDto.java
+++ src/main/java/br/com/zup/InvestimentoZup/dto/InvestimentoDto.java
@@ -5,13 +5,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 public class InvestimentoDto {
 
+    @NotBlank(message = "{validation.blank}")
+    @Email(message = "{validation.email}")
     private String email;
+    @NotBlank(message = "{validation.nome}")
     private String nome;
     private String telefone;
     private Long moneyAplicado;
